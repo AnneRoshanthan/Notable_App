5d8e09da62f154bd1df0376f474ca5f98720168f
AnneRoshanthan <anneroshanthan@gmail.com>
2021-03-16 15:59:03 +0530

    heroku app

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports={
+url :"mongodb://rosan:rosan12345@cluster0-shard-00-00.ztaun.mongodb.net:27017,cluster0-shard-00-01.ztaun.mongodb.net:27017,cluster0-shard-00-02.ztaun.mongodb.net:27017/notes?ssl=true&replicaSet=atlas-vx4etf-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
